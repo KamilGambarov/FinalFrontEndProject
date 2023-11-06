03cbf70e2dd28489215db3c5b8e1807e0d07a5fc
Kamil Gambarov <gambarovkamil@gmail.com>
2023-11-06 15:42:23 +0400

    added responsive creator info page

--- client/html/pages/creator_info/creator_info.js
+++ client/html/pages/creator_info/creator_info.js
@@ -10,11 +10,12 @@ let chainId = document.querySelector(".right_content .primaryBtn:first-child>spa
 let nftCards = document.querySelector(".cards");
 
 
+
 async function getCreatorInfo(){
     let response = await fetch(`http://localhost:3000/api/creators/${creatorId}`);
     let creatorInfo = await response.json();
     fillCreatorInfo(creatorInfo);
-    fillCreatorsNFTs(creatorInfo.nfts);
+    fillCreatorsNFTs(creatorInfo);
 }
 
 function fillCreatorInfo(creator){
@@ -27,27 +28,49 @@ function fillCreatorInfo(creator){
     chainId.textContent = creator.chainId.substring(0,6)+ "..." + creator.chainId.substring(creator.chainId.length-4,creator.chainId.length);
 }
 
-function fillCreatorsNFTs(nfts){
-    nfts.forEach(element => {
+function fillCreatorsNFTs(creator){
+    creator.nfts.forEach(element => {
     let card = document.createElement("div");
     let card_topside = document.createElement("div");
     card_topside.className = "topside";
     let card_topside_img = document.createElement("img");
     card_topside_img.src = `../../../../${element.imgPath}`;
-    console.log(card_topside_img.src);
     let card_bottomside = document.createElement("div");
     card_bottomside.className = "bottomside";
     let card_bottomside_h3 = document.createElement("h3");
     card_bottomside_h3.textContent = element.name;
     let card_bottomside_div1 = document.createElement("div");
-    let card_bottomside_div1_price = document.createElement("p");
+    let card_bottomside_div1_img = document.createElement("img");
+    card_bottomside_div1_img.src = `../../../../${creator.profileImgPath}`;
+    let card_bottomside_div1_p = document.createElement("p");
+    card_bottomside_div1_p.textContent = creator.name;
     let card_bottomside_div2 = document.createElement("div");
-    let card_bottomside_div2_highestBid = document.createElement("p");
+    let card_bottomside_div2_div1 = document.createElement("div");
+    let card_bottomside_div2_div2 = document.createElement("div");
+    let card_bottomside_div2_div1_price = document.createElement("p");
+    card_bottomside_div2_div1_price.textContent = "Price";
+    let card_bottomside_div2_div1_price_value = document.createElement("p");
+    card_bottomside_div2_div1_price_value = element.price.value + " " + element.price.currency;
+    let card_bottomside_div2_div2_highestBid = document.createElement("p");
+    card_bottomside_div2_div2_highestBid.textContent = "Highest Bid";
+    let card_bottomside_div2_div2_highestBid_value = document.createElement("p");
+    card_bottomside_div2_div2_highestBid_value.textContent = element.highestBid.value + " " + element.highestBid.currency;
+
     nftCards.append(card);
     card.append(card_topside);
     card_topside.append(card_topside_img);
     card.append(card_bottomside);
     card_bottomside.append(card_bottomside_h3);
+    card_bottomside.append(card_bottomside_div1);
+    card_bottomside.append(card_bottomside_div2);
+    card_bottomside_div1.append(card_bottomside_div1_img);
+    card_bottomside_div1.append(card_bottomside_div1_p);
+    card_bottomside_div2.append(card_bottomside_div2_div1);
+    card_bottomside_div2.append(card_bottomside_div2_div2); 
+    card_bottomside_div2_div1.append(card_bottomside_div2_div1_price);
+    card_bottomside_div2_div1.append(card_bottomside_div2_div1_price_value);
+    card_bottomside_div2_div2.append(card_bottomside_div2_div2_highestBid);
+    card_bottomside_div2_div2.append(card_bottomside_div2_div2_highestBid_value);
     });
 }
 
