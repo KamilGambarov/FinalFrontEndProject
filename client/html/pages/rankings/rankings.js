b3379a1f60547beb8ad0484df4a828ee8ee9f839
Kamil Gambarov <gambarovkamil@gmail.com>
2023-11-16 17:05:13 +0400

    responsive design added for rankings page

--- client/html/pages/rankings/rankings.js
+++ client/html/pages/rankings/rankings.js
@@ -14,7 +14,8 @@ function fillRankings(rankings){
         card.className = "card";
         let ranking_id = document.createElement("div");
         ranking_id.className = "number";
-        ranking_id.textContent = element.id;
+        let ranking_id_number = document.createElement("span");
+        ranking_id_number.textContent = element.id; 
         let ranking_img = document.createElement("img");
         ranking_img.src = `../../../../${element.profileImgPath}`;
         let ranking_name = document.createElement("div");
@@ -22,21 +23,45 @@ function fillRankings(rankings){
         ranking_name.textContent = element.name;
         let ranking_change = document.createElement("div");
         ranking_change.className = "change";
-        ranking_change.textContent = "1.41%";
+        ranking_change.textContent = element.totalSale.value + " " + element.totalSale.currency;
         let ranking_NFTs = document.createElement("div");
         ranking_NFTs.className = "NFTs_sold";
-        ranking_NFTs.textContent = "602";
+        ranking_NFTs.textContent = element.nftSold;
         let ranking_volume = document.createElement("div");
         ranking_volume.className = "volume";
-        ranking_volume.textContent = element.totalSale.value + " " + element.totalSale.currency;
+        ranking_volume.textContent = element.followers;
+        let deleteBtn = document.createElement("button");
+        deleteBtn.className = "primaryBtn";
+        deleteBtn.textContent = "Delete";
+        deleteBtn.style.width = "50px";
+        deleteBtn.style.position = "absolute";
+        deleteBtn.style.right = "10px";
 
         ranking_cards.append(card);
         card.append(ranking_id);
+        ranking_id.append(ranking_id_number);
         card.append(ranking_img);
         card.append(ranking_name);
         card.append(ranking_change);
         card.append(ranking_NFTs);
         card.append(ranking_volume);
+        card.append(deleteBtn);
+
+        deleteBtn.addEventListener("click", ()=>{
+            deleteCreator(element, card);
+        })
     });
+}
+
+
+async function deleteCreator(ranking, card){
+    if(confirm(`Are you sure to delete creator with id ${ranking.id}?`)){
+        let response = fetch(`http://localhost:3000/api/creators/${ranking.id}`, {
+            method: "DELETE",
+        });
+        if((await response).status == 200){
+            card.remove();
+        }
+    }
 
 }
